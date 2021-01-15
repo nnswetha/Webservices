adb351a59e9787dc823f6959abf7b07ff31b0cc8
nnswetha <nnswetha31997@gmail.com>
2021-01-15 13:49:04 +0530

    Firstcommit

---
+++ webservices1/src/main/java/com/javastage3/webservices1/Webservices1Application.java
@@ -0,0 +1,14 @@
+package com.javastage3.webservices1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webservices1Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(Webservices1Application.class, args);
+	}
+
+}
---
+++ webservices1/src/test/java/com/javastage3/webservices1/Webservices1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javastage3.webservices1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webservices1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
